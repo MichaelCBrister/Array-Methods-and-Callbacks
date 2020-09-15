92315a10054c045d93a04a528efbf0beeab2d964
MichaelCBrister <70389996+MichaelCBrister@users.noreply.github.com>
2020-09-15 16:00:16 -0500

    completed task 3

--- index.js
+++ index.js
@@ -20,22 +20,23 @@ console.log(fifaData[828]["Win conditions"]);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals() {
-    const onlyFinals = fifaData.filter((item) => {
+function getFinals(data) {
+    const onlyFinals = data.filter((item) => {
         return item.Stage == "Final"
     });
     return onlyFinals;
 }
-console.log(getFinals("data"));
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(getFinals) {
-    const years = fifaData.map((item) => {
-        return {"Year": item.Year};
+function getYears(callback) {
+    const years = callback.map(function (item) {
+      return {year: item.Year};
     });
-}
-console.log(getYears(getFinals()));
+    return years
+  }
+  console.log(getYears(getFinals(fifaData)));
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
